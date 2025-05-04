923698b4aacd9891f5abd3d0a26e6856f8ccfe3a
Anderson Souza <dev.andersondesouzafeitosa@gmail.com>
2025-05-04 00:37:23 -0300

    next/font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,17 @@
 import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Header } from "@/components/header";
 
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "Encurtadim",
@@ -29,7 +39,7 @@ export default function RootLayout({
   return (
     <html lang="pt-br">
       <body
-        className={`bg-[#232323] antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} bg-[#232323] antialiased`}
       >
         <div className="w-full max-w-5xl m-auto h-screen max-h-screen overflow-hidden">
           <Header />
